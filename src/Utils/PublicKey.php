d7e11f81cb2942ca1861730f5f060f77ff07b1d3
Matt Stauffer <matt@tighten.co>
2021-09-12 22:55:38 -0400

    Pass findProgramAddress through to JavaScript

--- src/Utils/PublicKey.php
+++ src/Utils/PublicKey.php
@@ -3,9 +3,28 @@
 namespace Tighten\SolanaPhpSdk\Utils;
 
 use Exception;
+use Symfony\Component\Process\ExecutableFinder;
+use Symfony\Component\Process\Process;
 
 class PublicKey
 {
+    public function findProgramAddressViaJavascript(string $mintKey)
+    {
+        $process = new Process([
+            // (new ExecutableFinder())->find('node'), // @todo solve why not working!
+            '/Users/mattstauffer/.nvm/versions/node/v15.4.0/bin/node',
+            'findProgramAddress.js',
+            $mintKey,
+        ], realpath(__DIR__ . '/../../js-src'), null, null, null);
+
+        $process->run();
+
+        if (! $process->isSuccessful()) {
+            dd($process->getErrorOutput());
+        }
+
+        return trim($process->getOutput());
+    }
     /**
      * Find a valid program address
      *
